267491aa06106c10a41e51ae81a39b637b3d3e80
yuccaaaa <yucayann5@yahoo.co.jp>
2017-10-05 17:42:30 +0900

    you can edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,5 +17,16 @@ class PostsController < ApplicationController
   end 
 
   def edit
+    @post = Post.find_by(id: params[:id])
   end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    @post.save
+    redirect_to("/posts/index")
+  end
+
+
+
 end
